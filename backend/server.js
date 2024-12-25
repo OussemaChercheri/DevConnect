f910e2a9ea7449dae1e1438efdb855cc001dd016
oussama chercheri <oussama.chercheri@tek-up.de>
2024-12-25 04:35:18 +0100

    add OAuth

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,55 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
+import { Strategy as GitHubStrategy } from 'passport-github2';
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
-import notificationRoutes from "./routes/notofication.routes.js";
-import connectionRoutes from "./routes/connection.routes.js";
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+import postRoutes from './routes/post.routes.js';
+import notificationRoutes from './routes/notofication.routes.js';
+import connectionRoutes from './routes/connection.routes.js';
 import { connectDB } from './lib/db.js';
-
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/posts", postRoutes);
-app.use("/api/v1/notifications", notificationRoutes);
-app.use("/api/v1/connections", connectionRoutes);
+// Session middleware (required for Passport)
+app.use(
+  session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-    
-})
+// Initialize Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure Passport GitHub Strategy
+
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/notifications', notificationRoutes);
+app.use('/api/v1/connections', connectionRoutes);
+
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
